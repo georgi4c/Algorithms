2268cc3c615065c9027c80cb4d0ea66769eafde7
Georgi Ivaylov Georgiev <georgi4c@gmail.com>
2017-08-12 21:12:11 +0300

    ProblemSolvingPart1Updated

---
+++ ProblemSolvingPart1/ElectionFast/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ElectionFast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controlBorder = int.Parse(Console.ReadLine());
+            var numberOfParties = int.Parse(Console.ReadLine());
+            var numbers = new int[numberOfParties];
+            for (int i = 0; i < numberOfParties; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            var sums = new BigInteger[numbers.Sum() + 1];
+            
+            sums[0] = 1;
+            foreach (var number in numbers)
+            {
+                for (int i = sums.Length - 1; i >= 0; i--)
+                {
+                    if (sums[i] != 0)
+                    {
+                        sums[i + number] += sums[i];
+                    }
+                }
+            }
+            BigInteger count = 0;
+            for (int i = controlBorder; i < sums.Length; i++)
+            {
+                count += sums[i];
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
--- ProblemSolvingPart1/Elections/Program.cs
+++ ProblemSolvingPart1/Elections/Program.cs
@@ -8,8 +8,69 @@ namespace Elections
 {
     class Program
     {
+        static int k;
+        static Dictionary<int, int> votes = new Dictionary<int, int>();
+        static int[] parties;
+        private static int controlBorder;
+        private static int numberOfParties;
+        //static List<string> result = new List<string>();
+        private static int result;
+        
+        static int[] slots;
+
         static void Main(string[] args)
         {
+            controlBorder = int.Parse(Console.ReadLine());
+            numberOfParties = int.Parse(Console.ReadLine());
+            parties= new int[numberOfParties];
+            for (int i = 0; i < numberOfParties; i++)
+            {
+                parties[i] = i;
+                votes[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i <= numberOfParties; i++)
+            {
+                slots = new int[i];
+                k = i;
+                GenerateCombinationsNoRepetitions(0, 0);
+            }
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.WriteLine(result);
         }
+
+        //static void Start()
+        //{
+        //    parties = Console.ReadLine().Split(' ').ToArray();
+        //    k = int.Parse(Console.ReadLine());
+        //    slots = new string[k];
+
+        //    GenerateCombinationsNoRepetitions(0, 0);
+        //}
+
+        static void GenerateCombinationsNoRepetitions(int index, int start)
+        {
+            if (index >= k)
+            {
+                if (slots.Select(s => votes[s]).Sum() >= controlBorder)
+                {
+                    //result.Add(string.Join(" ", slots));
+                    result++;
+                }
+            }
+            else
+            {
+                for (int i = start; i < parties.Length; i++)
+                {
+                    slots[index] = parties[i];
+                    GenerateCombinationsNoRepetitions(index + 1, i + 1);
+                }
+            }
+        }
+        
     }
+   
 }
